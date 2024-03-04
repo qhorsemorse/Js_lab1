88a93e512b1f42c773ed7e2576a2120982c77966
qhorsemorse <155101091+qhorsemorse@users.noreply.github.com>
2024-03-04 09:37:17 +0200

    Add files via upload

---
+++ lab_2/my_script.js
@@ -0,0 +1,264 @@
+let object = [
+	{
+		id: 1,
+		src: "img/photo_1_2024-03-03_08-37-41.jpg",
+		title: "Drone",
+		parameter: ["Photo", "Fun"],
+		price: 1000,
+	},
+
+	{
+		id: 2,
+		src: "img/photo_2_2024-03-03_08-37-41.jpg",
+		title: "Car",
+		parameter: ["Work", "Fun", "Home"],
+		price: 20000,
+	},
+
+	{
+		id: 3,
+		src: "img/photo_3_2024-03-03_08-37-41.jpg",
+		title: "Doorbell",
+		parameter: ["Home", "Photo"],
+		price: 100,
+	},
+
+	{
+		id: 4,
+		src: "img/photo_4_2024-03-03_08-37-41.jpg",
+		title: "VRset",
+		parameter: ["Home", "Fun"],
+		price: 300,
+	},
+
+	{
+		id: 5,
+		src: "img/photo_5_2024-03-03_08-37-41.jpg",
+		title: "Airpods",
+		parameter: ["Fun"],
+		price: 200,
+	},
+
+	{
+		id: 6,
+		src: "img/photo_6_2024-03-03_08-37-41.jpg",
+		title: "Phone",
+		parameter: ["Home", "Work", "Fun", "Photo"],
+		price: 1200,
+	},
+
+	{
+		id: 7,
+		src: "img/photo_7_2024-03-03_08-37-41.jpg",
+		title: "Watch",
+		parameter: ["Home", "Work"],
+		price: 700,
+	},
+
+	{
+		id: 8,
+		src: "img/photo_8_2024-03-03_08-37-41.jpg",
+		title: "Laptop",
+		parameter: ["Home", "Work", "Fun", "Photo"],
+		price: 3000,
+	},
+
+	{
+		id: 9,
+		src: "img/photo_9_2024-03-03_08-37-41.jpg",
+		title: "Console",
+		parameter: ["Fun", "Home"],
+		price: 400,
+	},
+
+	{
+		id: 10,
+		src: "img/question_mark_with_magnifier.jpg",
+		title: "Need more?",
+		parameter: ["Home", "Work", "Fun", "Photo"],
+		price: 0,
+	},
+
+	{
+		id: 11,
+		src: "img/Party.jpg",
+		title: "Party",
+		parameter: ["Fun"],
+		price: 1000,
+	},
+
+	{
+		id: 12,
+		src: "img/Home.jpg",
+		title: "Home",
+		parameter: ["Home"],
+		price: 1000000,
+	},
+
+	{
+		id: 13,
+		src: "img/Pen.jpg",
+		title: "Pen",
+		parameter: ["Work"],
+		price: 5,
+	},
+
+	{
+		id: 14,
+		src: "img/Camera.jpg",
+		title: "Camera",
+		parameter: ["Photo"],
+		price: 500,
+	},
+]
+
+
+function switchPage() {
+	window.location.href = 'Products.html';
+}
+
+
+function backToHome() {
+	window.location.href = 'redsite.html';
+}
+
+function AboutUs() {
+	window.location.href = 'About Us.html';
+}
+
+function inefficientReload(ID) {
+	var parentElement = document.getElementById(ID)
+	while (parentElement.firstChild) {
+		parentElement.removeChild(parentElement.firstChild);
+	}
+}
+
+
+
+function generateBox(title, theImage, price) {
+	var box = document.createElement('article');
+	box.classList.add('box');
+
+	var heading = document.createElement('h3');
+	heading.style.margin = "0"
+	heading.style.marginLeft = "20px"
+	heading.textContent = title;
+
+	var img = document.createElement('img');
+	img.setAttribute('src', theImage);
+	img.style.width = "200px"
+
+	var button = document.createElement('button');
+	button.setAttribute('type', 'button');
+	button.classList.add('button');
+	button.textContent = "More..."
+	button.addEventListener('click', function () {
+		recreateMore();
+	});
+	if (price != 0) {
+		button.addEventListener('click', function () {
+			window.location.href = 'https://www.youtube.com/shorts/SXHMnicI6Pg';
+		});
+		button.textContent = "Buy!";
+	}
+	button.style.marginTop = "20px"
+	button.style.marginBottom = "20px"
+
+	var p = document.createElement('h1');
+	UsdPrice = String(price) + "$";
+	p.textContent = UsdPrice;
+	p.style.marginTop = "20px"
+
+	box.appendChild(heading);
+	box.appendChild(img);
+	box.appendChild(button);
+	box.appendChild(p)
+
+	var container = document.getElementById("ThaBox0");
+
+	container.appendChild(box);
+}
+
+
+function recreateElements() {
+	inefficientReload("ThaBox0");
+	for (i = 0; i < 10; i++) {
+		title = object[i].title;
+		path = object[i].src;
+		price = object[i].price;
+		let Option = getOptionInfo();
+		if (sort_by(i, Option)) {
+			generateBox(title, path, price);
+		}
+	}
+}
+
+
+function getOptionInfo() {
+	var dropdown = document.getElementById("Dropdown");
+	var selectedOption = dropdown.options[dropdown.selectedIndex].value;
+	return selectedOption;
+}
+
+function sort_by(index, Option) {
+	if (Option == "All") {
+		return true;
+	}
+
+	return (object[index].parameter.includes(Option));
+}
+
+
+function recreateMore() {
+	var parentElement = document.getElementById("ThaBox0")
+	parentElement.removeChild(parentElement.lastChild);
+
+	for (i = 10; i < 14; i++) {
+		title = object[i].title;
+		path = object[i].src;
+		price = object[i].price;
+		let Option = getOptionInfo();
+		if (sort_by(i, Option)) {
+			generateBox(title, path, price);
+		}
+	}
+}
+
+
+
+
+
+
+var modal = document.getElementById("modal");
+
+var span = document.getElementsByClassName("close")[0];
+
+var button = document.getElementById("closeModal");
+
+function reloadModal() {
+	modal.style.display = "none";
+	setTimeout(function () {
+		document.getElementById('modal').style.display = "block";
+	}, 3000);
+}
+
+button.onclick = function () { closeModal(); }
+
+function closeModal() {
+	modal.style.display = "none";
+}
+
+window.onload = function () {
+	recreateElements();
+	if (!localStorage.getItem('buttonClicked')) {
+		modal.style.display = "block";
+	}
+}
+
+// if (localStorage.getItem('buttonClicked')) {
+// 	modal.style.display = "none";
+// }
+
+button.addEventListener('click', function () {
+	localStorage.setItem('buttonClicked', true);
+});
